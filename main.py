5d8d0282ada046180c7bda84a05a092cc80d1d73
soumen02 <sm8966@nyu.edu>
2023-11-16 15:23:24 +0400

    working reportfile

--- main.py
+++ main.py
@@ -33,82 +33,117 @@ def move_and_rename_files(source_path, output_path, prefix, file_suffix='.nii.gz
             new_filename = prefix + filename
             shutil.move(os.path.join(source_path, filename), os.path.join(output_path, new_filename))
 
+def validate_files_dict(files):
+    for key, value in files.items():
+        if not value:
+            logging.error(f"No files found for {key}.")
+            return False
+    return True
+
 def get_nii_file(directory):
                 for file in os.listdir(directory):
                     if file.endswith(".nii.gz"):
                         return file
                 return None
 
+def gather_files(path_key, result_key, label, paths, files):
+    # Ensure the paths exist
+    if not os.path.exists(paths[path_key]):
+        logging.error(f"Path {paths[path_key]} does not exist")
+        exit(1)
+    if not os.path.exists(paths[result_key]):
+        logging.error(f"Path {paths[result_key]} does not exist")
+        exit(1)
+    
+    # Get the .nii file for the study
+    study_file = get_nii_file(paths[path_key])
+    if not study_file:
+        logging.error(f"No .nii.gz file found in {paths[path_key]}")
+        return
+    
+    if study_file:
+        study_filepath = os.path.join(paths[path_key], study_file)
+        
+        # Move and rename files
+        move_and_rename_files(paths[result_key], paths["outputfolder"], 'label_')
+        
+        result_file = get_nii_file(paths[result_key])
+        if not result_file:
+            logging.error(f"No .nii.gz file found in {paths[result_key]}")
+            return
+        
+        if result_file:
+            result_filepath = os.path.join(paths[result_key], result_file)
+            
+            # Update the dictionary
+            files[label] = (study_filepath, result_filepath)
+
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    paths = {
-    "studiesHA": get_path(dir_path, "StudiesHA"),
-    "studiesPV": get_path(dir_path, "StudiesPV"),
-    "studiesHV": get_path(dir_path, "StudiesHV"),
-    "resultsHA": get_path(dir_path, "StudiesHA", "test_labels"),
-    "resultsPV": get_path(dir_path, "StudiesPV", "test_labels"),
-    "resultsHV": get_path(dir_path, "StudiesHV", "test_labels"),
-    "outputfolder": get_path(dir_path, "Output"),
-    "mainHA": get_path(dir_path, "radiologyHA", "main.py"),
-    "mainPV": get_path(dir_path, "radiologyPV", "main.py"),
-    "mainHV": get_path(dir_path, "radiologyHV", "main.py")
-}
-    ### 
+    ####
+
+#     paths = {
+#     "studiesHA": get_path(dir_path, "StudiesHA"),
+#     "studiesPV": get_path(dir_path, "StudiesPV"),
+#     "studiesHV": get_path(dir_path, "StudiesHV"),
+#     "resultsHA": get_path(dir_path, "StudiesHA", "test_labels"),
+#     "resultsPV": get_path(dir_path, "StudiesPV", "test_labels"),
+#     "resultsHV": get_path(dir_path, "StudiesHV", "test_labels"),
+#     "outputfolder": get_path(dir_path, "Output"),
+#     "mainHA": get_path(dir_path, "radiologyHA", "main.py"),
+#     "mainPV": get_path(dir_path, "radiologyPV", "main.py"),
+#     "mainHV": get_path(dir_path, "radiologyHV", "main.py")
+# }
+    
+#     # check the files in study and make sure they are all .nii.gz
+#     # if its .dcm, convert it to .nii.gz using dcm2niix
+#     # else if its .nii, leave it as is
+#     # else if its neither, log error and exit
+
+#     for directory in [paths["studiesHA"], paths["studiesPV"], paths["studiesHV"]]:
+#         for file in os.listdir(directory):
+#             if file.endswith(".dcm"):
+#                 run_command(["dcm2niix", "-o", directory, "-f", "%p", directory])
+#                 break
+#             elif file.endswith(".nii"):
+#                 break
+#             else:
+#                 logging.error(f"File {file} is not a .dcm or .nii file. Exiting.")
+#                 exit(1)
+
+
+ 
     # for key in ["mainHA", "mainPV", "mainHV"]:
     #     run_command(["python3", paths[key]])
 
-    files = {'Hepatic Artery':(),
-             'Portal Vein':(),
-             'Hepatic Vein':()}
+    # files = {'Hepatic Artery':(),
+    #          'Portal Vein':(),
+    #          'Hepatic Vein':()}
     
-    for key in ["studiesHA", "studiesPV", "studiesHV"]:
-        # move_and_rename_files(paths[key], paths["outputfolder"], 'volume_')
-        pass
-        
-        if not os.path.exists(paths[key]):
-            logging.error(f"Path {paths[key]} does not exist")
-            exit(1)
-        else:
-            if key == "studiesHA":
-                nii_file = get_nii_file(paths[key])
-                if nii_file:
-                    files['Hepatic Artery'] += (os.path.join(paths[key], nii_file),)
-            elif key == "studiesPV":
-                nii_file = get_nii_file(paths[key])
-                if nii_file:
-                    files['Portal Vein'] += (os.path.join(paths[key], nii_file),)
-            elif key == "studiesHV":
-                nii_file = get_nii_file(paths[key])
-                if nii_file:
-                    files['Hepatic Vein'] += (os.path.join(paths[key], nii_file),)
+    # # Gather files for each phase
+    # gather_files("studiesHA", "resultsHA", 'Hepatic Artery', paths, files)
+    # gather_files("studiesPV", "resultsPV", 'Portal Vein', paths, files)
+    # gather_files("studiesHV", "resultsHV", 'Hepatic Vein', paths, files)
 
-    for key in ["resultsHA", "resultsPV", "resultsHV"]:
-        if not os.path.exists(paths[key]):
-            logging.error(f"Path {paths[key]} does not exist")
-            exit(1)
-        else:
-            move_and_rename_files(paths[key], paths["outputfolder"], 'label_')
-
-            if key == "resultsHA":
-                files['Hepatic Artery'] += (os.path.join(paths["outputfolder"], os.listdir(paths["outputfolder"])[0]),)
-            elif key == "resultsPV":
-                files['Portal Vein'] += (os.path.join(paths["outputfolder"], os.listdir(paths["outputfolder"])[0]),)
-            elif key == "resultsHV":
-                files['Hepatic Vein'] += (os.path.join(paths["outputfolder"], os.listdir(paths["outputfolder"])[0]),)
-
-    # call reportscript.py to generate the report on the generated output folder, pass the files dictionary as an argument
-    run_command(["python3", get_path(dir_path, "reportscript.py"), paths["outputfolder"], str(files)])
-
-    # files = os.listdir(paths["outputfolder"])
-    # for file in files:
-    #     logging.info(file)
-    #     run_command(["fslstats", os.path.join(paths["outputfolder"], file), "-V"])
-
-    # if subprocess.run(["which", "slicer"], capture_output=True).returncode == 0:
-    #     run_command(["slicer", paths["outputfolder"]])
+    # if validate_files_dict(files):
+    #     run_command(["python3", get_path(dir_path, "reportscript.py"), paths["outputfolder"], str(files)])
     # else:
-    #     logging.warning("Slicer is not installed")
+    #     logging.error("Not all required files were found. Exiting.")
+
+    # ####
+
+    files = {'Hepatic Artery':('/home/mri/soumen_fall2023/LiverApp_noGUI/Output/volume_10153545_HA-PVreg.nii.gz', '/home/mri/soumen_fall2023/LiverApp_noGUI/Output/label_volume_10153545_HA-PVreg.nii.gz'),
+                'Portal Vein':('/home/mri/soumen_fall2023/LiverApp_noGUI/Output/volume_10153545_PV-PVreg.nii.gz', '/home/mri/soumen_fall2023/LiverApp_noGUI/Output/label_volume_10153545_PV-PVreg.nii.gz'),
+                'Hepatic Vein':('/home/mri/soumen_fall2023/LiverApp_noGUI/Output/volume_10153545_HV-PVreg.nii.gz', '/home/mri/soumen_fall2023/LiverApp_noGUI/Output/label_volume_10153545_HV-PVreg.nii.gz')
+                }
+    
+    run_command(["python3", get_path(dir_path, "reportscript.py"), '/home/mri/soumen_fall2023/LiverApp_noGUI/Output', str(files)])
+    
+
+
+
+
 
 if __name__ == "__main__":
     main()
--- reportscript.py
+++ reportscript.py
@@ -13,7 +13,14 @@ import SimpleITK as sitk
 import matplotlib.pyplot as plt
 from reportlab.pdfgen import canvas
 import numpy as np
+from PIL import Image
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Image as ReportLabImage, Spacer
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+from datetime import datetime
 
+from reportlab.platypus import Paragraph, PageBreak
+from reportlab.lib.styles import getSampleStyleSheet
 
 # Configure logging
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -27,13 +34,18 @@ output_folder = sys.argv[1]
 files = eval(sys.argv[2])
 
 # print the dictionary of files
-print(files)
+logging.info("Files Dictionary", files)
 
 # Dictionary format
 # {
 #     'Label Type': ('Path to the Volume File', 'Path to the Label File'),
 # }
 
+def resize_image(input_path, output_path, size):
+    # 'size' is a tuple (width, height)
+    image = Image.open(input_path)
+    image = image.resize(size, Image.ANTIALIAS)
+    image.save(output_path)
 
 def plot_slices(sitk_image, filename):
     array_image = sitk.GetArrayFromImage(sitk_image)
@@ -53,43 +65,42 @@ COLOR_MAP = {
     'Hepatic Vein': "Blues"
 }
 
-def extract_and_overlay_slices(volume, label, label_type):
-    print(f"Processing label type: {label_type}")
+def extract_and_overlay_slices(volume, label, label_type, view):
+    # Determine which slice to extract based on the view
+    if view == 'axial':
+        slice_idx = volume.shape[0] // 2
+        slice = volume[slice_idx, :, :]
+        label_slice = label[slice_idx, :, :]
+    elif view == 'coronal':
+        slice_idx = volume.shape[1] // 2
+        slice = volume[:, slice_idx, :]
+        label_slice = label[:, slice_idx, :]
+    elif view == 'sagittal':
+        slice_idx = volume.shape[2] // 2
+        slice = volume[:, :, slice_idx]
+        label_slice = label[:, :, slice_idx]
+    else:
+        raise ValueError(f"Unknown view type: {view}")
 
-    # Extract slices
-    axial = volume[volume.shape[0]//2, :, :]
-    coronal = volume[:, volume.shape[1]//2, :]
-    sagittal = volume[:, :, volume.shape[2]//2]
+    # Flip the slices if necessary to match the desired radiological view
+    slice = np.flip(slice, axis=0)
+    label_slice = np.flip(label_slice, axis=0)
 
-    # Assuming `axial_slice` is the 2D numpy array representing an axial slice:
-    axial = np.flip(axial, axis=1)  # This flips the image horizontally
+    # Overlay the label slice onto the image slice
+    # Assuming label_slice is a binary mask where 1 indicates the label
+    color_map = {
+        'Hepatic Artery': 'Reds',
+        'Portal Vein': 'Greens',
+        'Hepatic Vein': 'Blues'
+    }
+    overlay_color = color_map.get(label_type, 'gray')
 
-    # For sagittal or coronal slices, if they appear flipped vertically:
-    sagittal = np.flip(sagittal, axis=0)  # This flips the image vertically
-    coronal = np.flip(coronal, axis=0)
+    plt.imshow(slice, cmap='gray')
+    plt.imshow(np.ma.masked_where(label_slice == 0, label_slice), cmap=overlay_color, alpha=0.5)
+    plt.axis('off')
 
-    # Extract label slices
-    axial_label = label[label.shape[0]//2, :, :]
-    coronal_label = label[:, label.shape[1]//2, :]
-    sagittal_label = label[:, :, label.shape[2]//2]
-
-    fig, axs = plt.subplots(1, 3, figsize=(9, 3))
-
-    # Overlay and plot
-    axs[0].imshow(axial, cmap="gray")
-    axs[0].imshow(np.ma.masked_where(axial_label == 0, axial_label), cmap=COLOR_MAP[label_type], alpha=0.8)
-
-    axs[1].imshow(coronal, cmap="gray")
-    axs[1].imshow(np.ma.masked_where(coronal_label == 0, coronal_label), cmap=COLOR_MAP[label_type], alpha=0.8)
-
-    axs[2].imshow(sagittal, cmap="gray")
-    axs[2].imshow(np.ma.masked_where(sagittal_label == 0, sagittal_label), cmap=COLOR_MAP[label_type], alpha=0.8)
-
-    for ax in axs:
-        ax.axis('off')
-
-    output_path = f"temp_{label_type}.png"
-    plt.tight_layout()
+    # Save the overlay image
+    output_path = f"temp_{label_type}_{view}.png"
     plt.savefig(output_path, bbox_inches='tight', pad_inches=0)
     plt.close()
 
@@ -97,26 +108,61 @@ def extract_and_overlay_slices(volume, label, label_type):
 
 
 def generate_pdf(output_folder, files):
-    c = canvas.Canvas(os.path.join(output_folder, 'report.pdf'), pagesize=landscape(letter))
-    width, height = landscape(letter)
+    doc = SimpleDocTemplate(os.path.join(output_folder, 'report.pdf'), pagesize=letter)
+    elements = []
 
-    y_position = height - 1 * inch  # Start near the top of the page
+    # Define the image and table sizes
+    image_width = 120  # Smaller width for images
+    image_height = 120  # Smaller height for images
+    table_width = doc.width/2.0  # Half the width of the page
+    table_data = [['Phase', 'Volume (mm³)']]
+    
+    # Header
+    elements.append(Paragraph('Triple Phase Liver CT Scan Report', getSampleStyleSheet()['Title']))
+    elements.append(Spacer(1, 12))
     
     for label_type, (volume_file, label_file) in files.items():
+        # Process the images and resize them
         volume_image = sitk.GetArrayFromImage(sitk.ReadImage(volume_file))
         label_image = sitk.GetArrayFromImage(sitk.ReadImage(label_file))
 
-        overlay_path = extract_and_overlay_slices(volume_image, label_image, label_type)
+        # Generate and save separate views for each phase
+        views = ['axial', 'coronal', 'sagittal']
+        image_paths = []
 
-        # Add the overlay image to the PDF
-        c.drawInlineImage(overlay_path, 1 * inch, y_position - 3*inch, width - 2*inch, 3*inch)
-        y_position -= 3.5 * inch  # Shift downwards for the next image
+        for view in views:
+            overlay_path = extract_and_overlay_slices(volume_image, label_image, label_type, view)
+            resized_image_path = f"resized_{label_type}_{view}.png"
+            resize_image(overlay_path, resized_image_path, size=(image_width, image_height))
+            image_paths.append(resized_image_path)
 
+        # Create the row with different views for each phase
+        image_row = [ReportLabImage(path) for path in image_paths]
+        elements.append(Table([image_row], colWidths=[image_width] * len(views), rowHeights=[image_height]))
+        elements.append(Spacer(1, 12))  # Add some space between the image rows
+
+
+        # Compute volume and add to the table data
         label_volume = compute_volume(sitk.ReadImage(label_file))
-        text = f"Volume of {label_type}: {label_volume:.2f} mm^3"
-        c.drawString(1 * inch, y_position, text)
-        y_position -= 0.25 * inch  # Small shift for text
-
-    c.save()
+        table_data.append([label_type, f"{label_volume:.2f}"])
+    
+    # Create the table for volume data
+    volume_table = Table(table_data, colWidths=[table_width/2.0] * 2)
+    volume_table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('BOX', (0, 0), (-1, -1), 1, colors.black),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+    
+    elements.append(volume_table)
+    elements.append(Spacer(1, 12))
+    elements.append(Paragraph('Generated on: {}'.format(datetime.now().strftime('%Y-%m-%d')), getSampleStyleSheet()['Normal']))
+    
+    doc.build(elements)
 
 generate_pdf(output_folder, files)
